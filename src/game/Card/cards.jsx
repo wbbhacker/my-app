0e79eff2f2abde186414c13687b687bc04373667
binbin.wang <binbin.wang.data@funplus.com>
2024-03-01 18:22:52 +0800

    feat:card over

--- src/game/Card/cards.jsx
+++ src/game/Card/cards.jsx
@@ -1,10 +1,14 @@
 import card1_icon from './img/card1.svg';
+import card2_icon from './img/card2.svg';
+import card3_icon from './img/card3.svg';
 import { Card } from './component/cardCp';
 
 export function Card1(props) {
-    return (
-        <div>
-            <Card icon={card1_icon} {...props}></Card>
-        </div>
-    );
+    return <Card icon={card1_icon} {...props}></Card>;
+}
+export function Card2(props) {
+    return <Card icon={card2_icon} {...props}></Card>;
+}
+export function Card3(props) {
+    return <Card icon={card3_icon} {...props}></Card>;
 }
--- src/game/Card/index.jsx
+++ src/game/Card/index.jsx
@@ -1,9 +1,15 @@
 import style from './index.module.scss';
-import { Card1 } from './cards';
+import * as cards from './cards';
+
 const Card = (props) => {
     return (
         <div className="content">
-            <Card1 style={{ width: 2 }}></Card1>
+            <div className={style['card-list']}>
+                {Object.keys(cards).map((key) => {
+                    let Cp = cards[key];
+                    return <Cp style={{ width: 1 }}></Cp>;
+                })}
+            </div>
         </div>
     );
 };
