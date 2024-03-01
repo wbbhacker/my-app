ed4e8b73dce95a13fb60da04c8142d89aa8f51d1
binbin.wang <binbin.wang.data@funplus.com>
2024-03-01 18:49:31 +0800

    feat:黑桃

--- src/game/Card/cards.jsx
+++ src/game/Card/cards.jsx
@@ -1,6 +1,16 @@
 import card1_icon from './img/card1.svg';
 import card2_icon from './img/card2.svg';
 import card3_icon from './img/card3.svg';
+import card4_icon from './img/card4.svg';
+import card5_icon from './img/card5.svg';
+import card6_icon from './img/card6.svg';
+import card7_icon from './img/card7.svg';
+import card8_icon from './img/card8.svg';
+import card9_icon from './img/card9.svg';
+import card10_icon from './img/card10.svg';
+import card11_icon from './img/card11.svg';
+import card12_icon from './img/card12.svg';
+import card13_icon from './img/card13.svg';
 import { Card } from './component/cardCp';
 
 export function Card1(props) {
@@ -11,4 +21,34 @@ export function Card2(props) {
 }
 export function Card3(props) {
     return <Card icon={card3_icon} {...props}></Card>;
+}
+export function Card4(props) {
+    return <Card icon={card4_icon} {...props}></Card>;
+}
+export function Card5(props) {
+    return <Card icon={card5_icon} {...props}></Card>;
+}
+export function Card6(props) {
+    return <Card icon={card6_icon} {...props}></Card>;
+}
+export function Card7(props) {
+    return <Card icon={card7_icon} {...props}></Card>;
+}
+export function Card8(props) {
+    return <Card icon={card8_icon} {...props}></Card>;
+}
+export function Card9(props) {
+    return <Card icon={card9_icon} {...props}></Card>;
+}
+export function Card10(props) {
+    return <Card icon={card10_icon} {...props}></Card>;
+}
+export function Card11(props) {
+    return <Card icon={card11_icon} {...props}></Card>;
+}
+export function Card12(props) {
+    return <Card icon={card12_icon} {...props}></Card>;
+}
+export function Card13(props) {
+    return <Card icon={card13_icon} {...props}></Card>;
 }
