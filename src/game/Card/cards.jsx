ab8e601af88896ccd45d3c364a898fbe6f063fa2
binbin.wang <binbin.wang.data@funplus.com>
2024-03-01 18:58:59 +0800

    feat:添加红桃

--- src/game/Card/cards.jsx
+++ src/game/Card/cards.jsx
@@ -11,6 +11,21 @@ import card10_icon from './img/card10.svg';
 import card11_icon from './img/card11.svg';
 import card12_icon from './img/card12.svg';
 import card13_icon from './img/card13.svg';
+
+import card14_icon from './img/card14.svg';
+import card15_icon from './img/card15.svg';
+import card16_icon from './img/card16.svg';
+import card17_icon from './img/card17.svg';
+import card18_icon from './img/card18.svg';
+import card19_icon from './img/card19.svg';
+import card20_icon from './img/card20.svg';
+import card21_icon from './img/card21.svg';
+import card22_icon from './img/card22.svg';
+import card23_icon from './img/card23.svg';
+import card24_icon from './img/card24.svg';
+import card25_icon from './img/card25.svg';
+import card26_icon from './img/card26.svg';
+
 import { Card } from './component/cardCp';
 
 export function Card1(props) {
@@ -51,4 +66,43 @@ export function Card12(props) {
 }
 export function Card13(props) {
     return <Card icon={card13_icon} {...props}></Card>;
+}
+export function Card14(props) {
+    return <Card icon={card14_icon} {...props}></Card>;
+}
+export function Card15(props) {
+    return <Card icon={card15_icon} {...props}></Card>;
+}
+export function Card16(props) {
+    return <Card icon={card16_icon} {...props}></Card>;
+}
+export function Card17(props) {
+    return <Card icon={card17_icon} {...props}></Card>;
+}
+export function Card18(props) {
+    return <Card icon={card18_icon} {...props}></Card>;
+}
+export function Card19(props) {
+    return <Card icon={card19_icon} {...props}></Card>;
+}
+export function Card20(props) {
+    return <Card icon={card20_icon} {...props}></Card>;
+}
+export function Card21(props) {
+    return <Card icon={card21_icon} {...props}></Card>;
+}
+export function Card22(props) {
+    return <Card icon={card22_icon} {...props}></Card>;
+}
+export function Card23(props) {
+    return <Card icon={card23_icon} {...props}></Card>;
+}
+export function Card24(props) {
+    return <Card icon={card24_icon} {...props}></Card>;
+}
+export function Card25(props) {
+    return <Card icon={card25_icon} {...props}></Card>;
+}
+export function Card26(props) {
+    return <Card icon={card26_icon} {...props}></Card>;
 }
