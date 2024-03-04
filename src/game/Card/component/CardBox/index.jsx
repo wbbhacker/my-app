f43a90c834595725b7bf179353fd8914a6b7d4d0
MS-MIZZEMNUPFSH\Administrator <2404016117@qq.com>
2024-03-04 23:05:33 +0800

    a

--- src/game/Card/component/CardBox/index.jsx
+++ src/game/Card/component/CardBox/index.jsx
@@ -1,198 +1,256 @@
 import React, {
-    useEffect,
-    useState,
-    forwardRef,
-    useImperativeHandle,
-    useRef,
-    useLayoutEffect,
-    useCallback,
-} from 'react';
-import styleClass from './index.module.scss';
-import cardAll from '../cards';
-import ReactHammer from 'react-hammerjs';
+  useEffect,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+  useLayoutEffect,
+  useCallback,
+} from "react";
+import styleClass from "./index.module.scss";
+import cardAll from "../cards";
+import ReactHammer from "react-hammerjs";
+import { Button } from "antd-mobile";
 
 const CW = 1.5; // 重叠 x rem
 const CH = 1; // 凸显出来 x rem
 const WW = 5.5; // 宽度
 function CardBox(props, ref) {
-    /*********************布局*********************/
+  /*********************布局*********************/
 
-    const [height, setHeight] = useState(4);
-    const [width, setWidth] = useState(2);
-    const [rRight, setRRright] = useState(0);
+  const [height, setHeight] = useState(4);
+  const [width, setWidth] = useState(2);
+  const [rRight, setRRright] = useState(0);
 
-    useEffect(() => {
-        const w = (height * 360) / 540;
-        const r = (52 - 1) * w - (52 - 1) * CW;
-        setWidth(w);
-        setRRright(r);
-    }, [height]);
+  useEffect(() => {
+    const w = (height * 360) / 540;
+    const r = (52 - 1) * w - (52 - 1) * CW;
+    setWidth(w);
+    setRRright(r);
+  }, [height]);
 
-    /*********************卡牌顺序*********************/
-    const { sequence } = props; // 初始化牌的顺序
-    const [cardSequence, setCardSequence] = useState([]);
+  /*********************卡牌顺序*********************/
+  const { sequence } = props; // 初始化牌的顺序
+  const [cardSequence, setCardSequence] = useState([]);
 
-    useEffect(() => {
-        if (sequence.length > 0) {
-            setCardSequence(sequence);
-        }
-    }, [sequence]);
+  useEffect(() => {
+    if (sequence.length > 0) {
+      setCardSequence(sequence);
+    }
+  }, [sequence]);
 
-    /*********************滚动*********************/
-    const listRef = useRef();
-    const boxRef = useRef();
+  /*********************滚动*********************/
+  const listRef = useRef();
+  const boxRef = useRef();
 
-    useLayoutEffect(() => {
-        if (cardSequence.length > 0) {
-            boxRef.current.scrollLeft =
-                listRef.current.children[cardSequence.length - 1].offsetLeft;
-        }
-    }, [cardSequence]);
+  useLayoutEffect(() => {
+    if (cardSequence.length > 0) {
+      boxRef.current.scrollLeft =
+        listRef.current.children[cardSequence.length - 1].offsetLeft;
+    }
+  }, [cardSequence]);
 
-    /*********************hook*********************/
-    const [selectedCard, setSelectedCard] = useState(undefined);
-    const { onCb } = props;
+  /*********************hook*********************/
+  const [selectedCard, setSelectedCard] = useState(undefined);
+  const { onCb } = props;
+  const [op, setOp] = useState(0);
 
-    useImperativeHandle(
-        ref,
-        () => {
-            return {
-                add: (CardIdx) => {
-                    // 添加卡牌
-                    console.log(`添加卡牌：${CardIdx}`);
-                    if (selectedCard === undefined) {
-                        cardSequence.push(CardIdx);
-                        console.log(cardSequence);
-                        setCardSequence([...cardSequence]);
-                    } else {
-                        onCb(cardSequence[selectedCard]);
-                        cardSequence.splice(selectedCard, 1, CardIdx);
-                        setSelectedCard(undefined);
-                    }
-                    // console.log(CardIdx);
-                },
-                end: (cb) => {
-                    cb([...cardSequence]);
-
-                    setCardSequence([]);
-                },
-            };
-        },
-        [selectedCard, cardSequence]
-    );
-
-    /*********************callback*********************/
-    // 双击选择
-
-    const handleDoubleTap = (Card, idx) => {
-        if (selectedCard === idx) {
-            setSelectedCard(undefined);
-        } else {
-            console.log(`selected：${idx}`);
-            setSelectedCard(idx);
-        }
-        console.log(`double top：${idx}`);
-    };
-
-    // 按压删除
-    const { onlyShow } = props;
-
-    useLayoutEffect(() => {
-        if (onlyShow) {
-            boxRef.current.scrollLeft = 40000;
-        }
-    }, [cardSequence]);
-    const onPress = (idx) => {
-        console.log(`Press：${idx}`);
-        console.log(selectedCard);
-        if (onlyShow) return;
-        if (idx !== selectedCard) {
-            onCb(cardSequence[idx]);
-            cardSequence.splice(idx, 1);
+  const opAdd = (v) => {
+    console.log(v);
+    setOp(v);
+  };
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        add: (CardIdx) => {
+          // 添加卡牌
+          console.log(`添加卡牌：${CardIdx}`);
+          console.log(`selectedCard:   ${selectedCard}`);
+          console.log(`op:   ${op}`);
+          if (selectedCard === undefined) {
+            cardSequence.push(CardIdx);
             setCardSequence([...cardSequence]);
-            if (selectedCard !== undefined) {
-                if (selectedCard > idx) {
-                    setSelectedCard(selectedCard - 1);
-                }
+          } else {
+            if (op === 0) {
+              onCb(cardSequence[selectedCard]);
+              cardSequence.splice(selectedCard, 1, CardIdx);
+              setSelectedCard(undefined);
+            } else {
+              console.log(op);
+              if (op === -1) {
+                //后插
+                cardSequence.splice(selectedCard + 1, 0, CardIdx);
+                setCardSequence([...cardSequence]);
+                setSelectedCard(undefined);
+                setOp(0);
+              }
+
+              if (op === 1) {
+                //后插
+                cardSequence.splice(selectedCard, 0, CardIdx);
+                setCardSequence([...cardSequence]);
+                setSelectedCard(undefined);
+                setOp(0);
+              }
             }
+          }
+          // console.log(CardIdx);
+        },
+        end: (cb) => {
+          cb([...cardSequence]);
+
+          setCardSequence([]);
+        },
+      };
+    },
+    [selectedCard, cardSequence, op]
+  );
+
+  /*********************callback*********************/
+  // 双击选择
+
+  const handleDoubleTap = (Card, idx) => {
+    if (selectedCard === idx) {
+      setSelectedCard(undefined);
+    } else {
+      //   console.log(`selected：${idx}`);
+      setSelectedCard(idx);
+    }
+    // console.log(`double top：${idx}`);
+  };
+
+  // 按压删除
+  const { onlyShow } = props;
+
+  useLayoutEffect(() => {
+    if (onlyShow) {
+      boxRef.current.scrollLeft = 40000;
+    }
+  }, [cardSequence]);
+  const onPress = (idx) => {
+    console.log(`Press：${idx}`);
+    console.log(selectedCard);
+    if (onlyShow) return;
+    if (idx !== selectedCard) {
+      onCb(cardSequence[idx]);
+      cardSequence.splice(idx, 1);
+      setCardSequence([...cardSequence]);
+      if (selectedCard !== undefined) {
+        if (selectedCard > idx) {
+          setSelectedCard(selectedCard - 1);
         }
-    };
+      }
+    }
+  };
 
-    /*********************render*********************/
+  /*********************render*********************/
 
-    const renderCard = (idx) => {
-        let CardIdx = cardSequence[idx];
-        let Card = cardAll[CardIdx];
-        return <Card style={{ width: width }}></Card>;
-    };
+  const renderCard = (idx) => {
+    let CardIdx = cardSequence[idx];
+    let Card = cardAll[CardIdx];
+    return <Card style={{ width: width }}></Card>;
+  };
 
-    return (
-        <div
-            ref={boxRef}
-            className={styleClass['card-box']}
-            style={{ height: `${height + CH}rem`, width: `${WW}rem` }}
-        >
-            <div
-                ref={listRef}
-                className={styleClass['card-list']}
-                style={{
-                    width: `${width * 52 - CW * 51}rem`,
+  return (
+    <div
+      ref={boxRef}
+      className={styleClass["card-box"]}
+      style={{ height: `${height + CH}rem`, width: `${WW}rem` }}
+    >
+      <div
+        ref={listRef}
+        className={styleClass["card-list"]}
+        style={{
+          width: `${width * 52 - CW * 51}rem`,
+          height: `${height}rem`,
+          marginTop: `${CH}rem`,
+        }}
+      >
+        {/* 最右边为第一张扑克 */}
+        {cardAll.map((Card, idx) => {
+          return (
+            <>
+              {cardSequence[idx] !== undefined ? (
+                <ReactHammer
+                  onDoubleTap={() => {
+                    handleDoubleTap(Card, idx);
+                  }}
+                  onPress={() => {
+                    onPress(idx);
+                  }}
+                  key={idx}
+                  options={{
+                    touchAction: "pan-x pan-y",
+                  }}
+                >
+                  <div
+                    style={{
+                      width: `${width}rem`,
+                      zIndex: 52 - idx,
+                      left: `${rRight - idx * (width - CW)}rem`,
+                      height: `${height}rem`,
+                      top: `-${selectedCard === idx ? CH / 2 : 0}rem`,
+                    }}
+                  >
+                    {selectedCard === idx ? (
+                      <div
+                        className={styleClass.op}
+                        style={{ top: `-${CH / 2}rem` }}
+                      >
+                        <Button
+                          color="primary"
+                          fill={op === -1 ? "solid" : "outline"}
+                          onClick={() => {
+                            opAdd(-1);
+                          }}
+                          size="mini"
+                        >
+                          {`后插`}
+                        </Button>
+                        <Button
+                          color="primary"
+                          fill={op === 0 ? "solid" : "outline"}
+                          onClick={() => {
+                            opAdd(0);
+                          }}
+                          size="mini"
+                        >
+                          {`替换`}
+                        </Button>
+                        <Button
+                          color="primary"
+                          fill={op === 1 ? "solid" : "outline"}
+                          onClick={() => {
+                            opAdd(1);
+                          }}
+                          size="mini"
+                        >
+                          {`前插`}
+                        </Button>
+                      </div>
+                    ) : null}
+
+                    {renderCard(idx)}
+                  </div>
+                </ReactHammer>
+              ) : (
+                <div
+                  key={idx}
+                  style={{
+                    width: `${width}rem`,
+                    zIndex: 52 - idx,
+                    left: `${rRight - idx * (width - CW)}rem`,
                     height: `${height}rem`,
-                    marginTop: `${CH}rem`,
-                }}
-            >
-                {/* 最右边为第一张扑克 */}
-                {cardAll.map((Card, idx) => {
-                    return (
-                        <>
-                            {cardSequence[idx] !== undefined ? (
-                                <ReactHammer
-                                    onDoubleTap={() => {
-                                        handleDoubleTap(Card, idx);
-                                    }}
-                                    onPress={() => {
-                                        onPress(idx);
-                                    }}
-                                    key={idx}
-                                    options={{
-                                        touchAction: 'pan-x pan-y',
-                                    }}
-                                >
-                                    <div
-                                        style={{
-                                            width: `${width}rem`,
-                                            zIndex: 52 - idx,
-                                            left: `${
-                                                rRight - idx * (width - CW)
-                                            }rem`,
-                                            height: `${height}rem`,
-                                            top: `-${
-                                                selectedCard === idx ? CH : 0
-                                            }rem`,
-                                        }}
-                                    >
-                                        {renderCard(idx)}
-                                    </div>
-                                </ReactHammer>
-                            ) : (
-                                <div
-                                    key={idx}
-                                    style={{
-                                        width: `${width}rem`,
-                                        zIndex: 52 - idx,
-                                        left: `${
-                                            rRight - idx * (width - CW)
-                                        }rem`,
-                                        height: `${height}rem`,
-                                    }}
-                                ></div>
-                            )}
-                        </>
-                    );
-                })}
-            </div>
-        </div>
-    );
+                  }}
+                ></div>
+              )}
+            </>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default React.memo(forwardRef(CardBox));
--- src/game/Card/pages/Page4/index.jsx
+++ src/game/Card/pages/Page4/index.jsx
@@ -1,150 +1,145 @@
 import React, {
-    useEffect,
-    useState,
-    forwardRef,
-    useImperativeHandle,
-    useRef,
-    useLayoutEffect,
-} from 'react';
-import _ from 'lodash';
+  useEffect,
+  useState,
+  forwardRef,
+  useImperativeHandle,
+  useRef,
+  useLayoutEffect,
+} from "react";
+import _ from "lodash";
 
-import { Button } from 'antd-mobile';
-import styleClass from './index.module.scss';
-import Timer from '../../component/Timer';
-import CardBox from '../../component/CardBox';
-import CardSquare from '../../component/CardSquare';
+import { Button } from "antd-mobile";
+import styleClass from "./index.module.scss";
+import Timer from "../../component/Timer";
+import CardBox from "../../component/CardBox";
+import CardSquare from "../../component/CardSquare";
 
 const Page4 = (props, ref) => {
-    const { onNext, onPrev, currentNumber, allNumber, curSequence } = props;
-    const [sequence, setSequence] = useState([]);
-    const cardBoxRef = useRef();
+  const { onNext, onPrev, currentNumber, allNumber, curSequence } = props;
+  const [sequence, setSequence] = useState([]);
+  const cardBoxRef = useRef();
 
-    const timerRef = useRef();
-    const cardSquareRef = useRef();
+  const timerRef = useRef();
+  const cardSquareRef = useRef();
 
-    /*********************callback*********************/
+  /*********************callback*********************/
 
-    useImperativeHandle(
-        ref,
-        () => {
-            return {
-                start: (cb) => {
-                    timerRef.current.start();
-                },
-            };
+  useImperativeHandle(
+    ref,
+    () => {
+      return {
+        start: (cb) => {
+          timerRef.current.start();
         },
-        []
-    );
+      };
+    },
+    []
+  );
 
-    useEffect(() => {
-        if (curSequence) {
-            setSequence(curSequence);
-        }
-    }, [curSequence]);
+  useEffect(() => {
+    if (curSequence) {
+      setSequence(curSequence);
+    }
+  }, [curSequence]);
 
-    /*********************callback*********************/
+  /*********************callback*********************/
 
-    // 卡牌选择
-    const cardSelect = (Card, cardIdx) => {
-        cardBoxRef.current.add(cardIdx);
-    };
-    // 卡牌回填
-    const onAdd = () => {};
+  // 卡牌选择
+  const cardSelect = (Card, cardIdx) => {
+    console.log("卡牌选择");
+    cardBoxRef.current.add(cardIdx);
+  };
+  // 卡牌回填
+  const onAdd = () => {};
 
-    // 卡牌回填
-    const onCb = (idx) => {
-        cardSquareRef.current.add(idx);
-    };
+  // 卡牌回填
+  const onCb = (idx) => {
+    cardSquareRef.current.add(idx);
+  };
 
-    // 作答完成
-    const [last, setLast] = useState(false);
-    useEffect(() => {
-        if (currentNumber !== undefined && allNumber !== undefined) {
-            setLast(currentNumber + 1 === allNumber);
-        }
-    }, [currentNumber, allNumber]);
+  // 作答完成
+  const [last, setLast] = useState(false);
+  useEffect(() => {
+    if (currentNumber !== undefined && allNumber !== undefined) {
+      setLast(currentNumber + 1 === allNumber);
+    }
+  }, [currentNumber, allNumber]);
 
-    // 上一副
-    const prevHandle = () => {
-        if (last) {
-            cardBoxRef.current.end((a) => {
-                onPrev(a);
-            });
-        } else {
-            onPrev();
-        }
-    };
-    // 下一幅
-    const nextHandle = () => {
-        if (last) {
-            let arr = [];
-            cardBoxRef.current.end((a) => {
-                arr = a;
-            });
-            timerRef.current.end((m) => {
-                onNext(m, arr);
-            });
-        } else {
-            cardBoxRef.current.end((a) => {
-                onNext(null, a);
-            });
-        }
+  // 上一副
+  const prevHandle = () => {
+    if (last) {
+      cardBoxRef.current.end((a) => {
+        onPrev(a);
+      });
+    } else {
+      onPrev();
+    }
+  };
+  // 下一幅
+  const nextHandle = () => {
+    if (last) {
+      let arr = [];
+      cardBoxRef.current.end((a) => {
+        arr = a;
+      });
+      timerRef.current.end((m) => {
+        onNext(m, arr);
+      });
+    } else {
+      cardBoxRef.current.end((a) => {
+        onNext(null, a);
+      });
+    }
 
-        cardSquareRef.current.end();
-    };
+    cardSquareRef.current.end();
+  };
 
-    return (
-        <div className={styleClass.page}>
-            <div className={styleClass.header}>
-                <div>
-                    <Timer ref={timerRef}></Timer>
-                </div>
-                {allNumber > 1 ? (
-                    <div className={styleClass.number}>
-                        第
-                        <span style={{ color: 'red' }}>
-                            {currentNumber + 1}
-                        </span>
-                        副
-                    </div>
-                ) : <div></div>}
-                <div>
-                    {currentNumber !== 0 ? (
-                        <Button
-                            color="primary"
-                            fill="outline"
-                            onClick={prevHandle}
-                            size="mini"
-                        >
-                            {`上一副`}
-                        </Button>
-                    ) : null}
-                    <Button
-                        color="primary"
-                        fill="outline"
-                        onClick={nextHandle}
-                        size="mini"
-                    >
-                        {last ? `作答完成` : `下一幅`}
-                    </Button>
-                </div>
-            </div>
-            <div className={styleClass.body}>
-                <CardBox
-                    sequence={sequence}
-                    ref={cardBoxRef}
-                    onCb={onCb}
-                ></CardBox>
-                <div className={styleClass.line}></div>
-                <CardSquare
-                    sequence={sequence}
-                    ref={cardSquareRef}
-                    onClick={cardSelect}
-                    onAdd={onAdd}
-                ></CardSquare>
-            </div>
+  return (
+    <div className={styleClass.page}>
+      <div className={styleClass.header}>
+        <div>
+          <Timer ref={timerRef}></Timer>
         </div>
-    );
+        {allNumber > 1 ? (
+          <div className={styleClass.number}>
+            第<span style={{ color: "red" }}>{currentNumber + 1}</span>副
+          </div>
+        ) : (
+          <div></div>
+        )}
+        <div>
+          {currentNumber !== 0 ? (
+            <Button
+              color="primary"
+              fill="outline"
+              onClick={prevHandle}
+              size="mini"
+            >
+              {`上一副`}
+            </Button>
+          ) : null}
+          <Button
+            color="primary"
+            fill="outline"
+            onClick={nextHandle}
+            size="mini"
+          >
+            {last ? `作答完成` : `下一幅`}
+          </Button>
+        </div>
+      </div>
+      <div className={styleClass.body}>
+        <CardBox sequence={sequence} ref={cardBoxRef} onCb={onCb}></CardBox>
+        <div className={styleClass.line}></div>
+        <CardSquare
+          sequence={sequence}
+          ref={cardSquareRef}
+          onClick={cardSelect}
+          onAdd={onAdd}
+        ></CardSquare>
+      </div>
+    </div>
+  );
 };
 
 export default React.memo(forwardRef(Page4));
