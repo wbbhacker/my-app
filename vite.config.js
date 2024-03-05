9bbc772857c33dca95201740b1395ab82459bc7b
binbin.wang <binbin.wang.data@funplus.com>
2024-03-05 10:38:22 +0800

    s

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     commonjsOptions: {
-      transformMixedEsModules: true,
+      // transformMixedEsModules: true,
     },
   },
 });
